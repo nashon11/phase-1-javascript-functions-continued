0c6e20f52c85bae6daf8a0ca31c0a7af78dbe854
NASH <nashon.mwenda@student.moringaschool.com>
2023-06-18 08:07:46 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+// Function declaration: saturdayFun
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Function expression: mondayWork
+  const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // Function definition: wrapAdjective
+  function wrapAdjective(flair = '*') {
+    return function(adjective) {
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+  }
+  // code your solution here
